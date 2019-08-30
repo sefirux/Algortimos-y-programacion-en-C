da6fb89091dbf227d609e2e11bd87f33ef7d5cc4
P-Jonathan <54717259+P-Jonathan@users.noreply.github.com>
2019-08-30 14:13:16 -0300

    Corrigiendo
    Se corrigieron los archivos de pruebas y makefile.

--- Estructuras de datos/TDA Pila/pruebas.c
+++ Estructuras de datos/TDA Pila/pruebas.c
@@ -4,9 +4,6 @@
 
 int main(void){
 	pila_t* pila = crear_pila();
-	if(pila == NULL){
-		return 1;
-	}
 
 	char vector_de_caracteres[] = {'H','O','L','A',' ','M','U','N','D','O','!'};
 	size_t tam = strlen(vector_de_caracteres);
@@ -26,6 +23,36 @@ int main(void){
 		printf("\tTamaño: %i\n\n", pila->tamanio);
 	}
 
+	destruir_pila(pila);
+
+	pila = crear_pila();
+	
+	char elemento_1 = '2';
+	char elemento_2 = 'o';
+	char elemento_3 = 'g';
+	char elemento_4 = 'l';
+	char elemento_5 = 'A';
+	
+	for (int i = 0; i < 3; i++) {
+		apilar(pila, &elemento_1);
+		apilar(pila, &elemento_2);
+		apilar(pila, &elemento_3);
+		apilar(pila, &elemento_4);
+		apilar(pila, &elemento_5);
+		printf("Tamanio pila: %i\n", pila->tamanio);
+	}
+	
+	for (int i = 0; i < 5; i++) {
+		printf("%c\n", *(char*)tope(pila));
+		desapilar(pila);
+	}
+	printf("Tamanio pila: %i\n", pila->tamanio);
+	
+	for (int i = 0; i < 5; i++) {
+		desapilar(pila);
+	}
+	printf("Tamanio pila: %i\n", pila->tamanio);
+	
 	destruir_pila(pila);
 
 	return 0;
